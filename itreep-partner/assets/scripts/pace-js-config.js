6d141b291f9347ac4c9b803befdb3d1e1721ad82
Nader Debbabi <nader@Naders-MacBook-Pro.local>
2019-02-10 20:52:20 +0100

    Auto-generated commit

---
+++ itreep-partner/assets/scripts/pace-js-config.js
@@ -0,0 +1,9 @@
+window.paceOptions = {
+    document: true,
+    eventLag: true,
+    restartOnPushState: true,
+    restartOnRequestAfter: true,
+    ajax: {
+        trackMethods: [ 'GET', 'PUT', 'PATCH', 'POST', 'DELETE' ]
+    }
+};
